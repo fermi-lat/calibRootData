90314ae9780f3590af9132787084bc34f1052c08
jrb <>
2004-07-21 19:09:38 +0000

    Embryonic mainpage

---
+++ src/mainpage.h
@@ -0,0 +1,17 @@
+// Mainpage for doxygen
+
+/** @mainpage package calibRootData
+  @author Joanne Bogart
+  @section intro Introduction
+  This package contains Root classes used in conversion of calibration
+  Root files to TDS objects 
+
+  @section requirements requirements
+  @include requirements
+  <hr>
+  @section notes release.notes
+  release.notes
+  <hr> 
+  @todo    Define lots more classes for different calibration types
+ */
+
