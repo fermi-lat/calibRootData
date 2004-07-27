e6a48bbeb08ec96e7db7d19c962fc442c28ed47d
jrb <>
2004-07-27 05:30:45 +0000

    Add +'s

--- calibRootData/LinkDef.h
+++ calibRootData/LinkDef.h
@@ -12,9 +12,9 @@
 
 // Probably have to do something special for namespace qualifier  calibRootData
 #pragma link C++ namespace calibRootData;
-#pragma link C++ class calibRootData::CalDimension;
-#pragma link C++ class calibRootData::CalGain;
-#pragma link C++ class calibRootData::DacCol;
-#pragma link C++ class calibRootData::CalGainCol;
-#pragma link C++ class calibRootData::CalSerial;
+#pragma link C++ class calibRootData::CalDimension+;
+#pragma link C++ class calibRootData::CalGain+;
+#pragma link C++ class calibRootData::DacCol+;
+#pragma link C++ class calibRootData::CalGainCol+;
+#pragma link C++ class calibRootData::CalSerial+;
 #endif
