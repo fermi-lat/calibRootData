86102c3a2f79af951a0dbeb5a598392dad9719f0
jrb <>
2004-12-17 18:38:47 +0000

    Added doc

--- src/mainpage.h
+++ src/mainpage.h
@@ -6,6 +6,11 @@
   This package contains Root classes used in conversion of calibration
   Root files to TDS objects 
 
+  Typical TKR files, like ToT charge injection calibration, will be composed
+  of one tree per tower. The tree will have two branches, one with a single
+  TkrTower object, the other with a set of objects, each of which has
+  data for a single unilayer.  See, for example, the class TotUnilayer.
+
   @section requirements requirements
   @include requirements
   <hr>
