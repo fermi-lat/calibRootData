135ad46f3a9eeea8e7f912a2d4fbbe5839bcde9a
glast <>
2008-07-11 00:32:27 +0000

    Changes to allow GR packages to build on windows

--- calibRootDataLib.py
+++ calibRootDataLib.py
@@ -2,6 +2,7 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibRootData'])
+    env.Tool('commonRootDataLib')
 
 def exists(env):
     return 1;
