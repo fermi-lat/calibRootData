aa41cd3c5854c09baa2bd3afd6588644e94f64bb
ecephas <>
2008-12-09 00:08:40 +0000

    Check in calibRootDataLib.py for calibRootData v1r7p8 (branch-tag, GlastReleasev15r49).

--- calibRootDataLib.py
+++ calibRootDataLib.py
@@ -3,6 +3,7 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibRootData'])
     env.Tool('commonRootDataLib')
-
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
