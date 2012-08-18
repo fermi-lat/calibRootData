33f971957878a9d5f35d0c7a673976857b926f1c
jrb <>
2012-08-18 00:18:31 +0000

    support no-install headers on Windows

--- calibRootDataLib.py
+++ calibRootDataLib.py
@@ -2,6 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'calibRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['rootLibs'])
